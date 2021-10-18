b6c6a489c9087ba792e44609b06334137fcc9a47
DongJun Yang <qza012@naver.com>
2021-10-19 01:32:26 +0900

    Merge pull request #1
    * 프로젝트 초기화

---
+++ src/main/java/com/study/yesmarket/YesMarketApplication.java
@@ -0,0 +1,13 @@
+package com.study.yesmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesMarketApplication.class, args);
+    }
+
+}
