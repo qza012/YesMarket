bc47b050f15d6a5f648286243ade4488f1349144
qza012 <qza012@naver.com>
2021-10-18 23:59:25 +0900

    프로젝트 초기화

---
+++ src/test/java/com/study/yesmarket/YesMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.yesmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
