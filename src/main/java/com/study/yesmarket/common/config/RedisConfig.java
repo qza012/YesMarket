6df3e00077f04c5102926b829e6d63b2e0e64e58
qza012 <wxs012@gmail.com>
2021-11-11 17:54:27 +0900

    feat : redis config 파일 추가

---
+++ src/main/java/com/study/yesmarket/common/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.study.yesmarket.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @EnableRedisHttpSession : properties 파일에 spring.session.store-type=redis 를 한 것과 동일한 동작을 한다.
+ */
+
+@EnableRedisHttpSession
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+}
