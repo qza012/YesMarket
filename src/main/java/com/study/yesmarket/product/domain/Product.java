24f68f35bb2de11f0dd585b0d79399a7a3fb3db9
qza012 <wxs012@gmail.com>
2021-11-15 14:08:33 +0900

    feat : product entity 생성

---
+++ src/main/java/com/study/yesmarket/product/domain/Product.java
@@ -0,0 +1,36 @@
+package com.study.yesmarket.product.domain;
+
+import com.study.yesmarket.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "product")
+@Entity
+public class Product extends BaseEntity {
+
+    @Id
+    private int productId;
+
+    private String name;
+
+    private int price;
+
+    private int stock;
+
+    @Builder
+    public Product(int productId, String name, int price, int stock) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+}
