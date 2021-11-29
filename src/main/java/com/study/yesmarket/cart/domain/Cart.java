e12a052f5ee80cad702976c5ac64cead1ff7adf6
qza012 <wxs012@gmail.com>
2021-11-30 02:11:12 +0900

    feature : cart 도메인 추가

--- src/main/java/com/study/yesmarket/cart/domain/Cart.java
+++ src/main/java/com/study/yesmarket/cart/domain/Cart.java
@@ -1,17 +1,43 @@
 package com.study.yesmarket.cart.domain;
 
+import com.study.yesmarket.common.domain.BaseEntity;
 import com.study.yesmarket.member.domain.Member;
+import com.study.yesmarket.product.domain.Product;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Set;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Cart {
+public class Cart extends BaseEntity {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    private int cartId;
+    private Integer cartId;
 
     @OneToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    @ManyToMany
+    @JoinTable(name = "cart_product")
+    private Set<Product> products;
+
+    @Builder
+    public Cart(int cartId) {
+        this.cartId = cartId;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
 }
