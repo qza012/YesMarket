7db0387f11a16749d85fbc1f4f4aa79d10f057ae
qza012 <wxs012@gmail.com>
2021-12-03 21:56:57 +0900

    refactor : error code 이동

--- src/main/java/com/study/yesmarket/member/exception/MemberErrorCode.java
+++ src/main/java/com/study/yesmarket/member/exception/MemberErrorCode.java
@@ -7,7 +7,9 @@ public enum MemberErrorCode implements BusinessErrorCode {
 
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임이 있습니다."),
     DUPLICATE_ID(HttpStatus.BAD_REQUEST, "중복된 아이디가 있습니다."),
-    NotFind_Member(HttpStatus.BAD_REQUEST, "해당 아이디의 유저가 존재하지 않습니다.");
+    NotFind_Member(HttpStatus.BAD_REQUEST, "해당 아이디의 유저가 존재하지 않습니다."),
+    NotMatchedId(HttpStatus.BAD_REQUEST, "유저 아이디가 일치하지 않습니다."),
+    NotMatchedPassword(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
     private HttpStatus status;
     private String message;
