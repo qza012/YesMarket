94de7d99602b1ef2a3231c079f284a8012d41bd9
qza012 <wxs012@gmail.com>
2021-12-03 23:35:51 +0900

    refactor : 옮긴 파일들 삭제

--- src/main/java/com/study/yesmarket/login/domain/SessionLoginRepository.java
+++
@@ -1,6 +0,0 @@
-package com.study.yesmarket.login.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionLoginRepository extends JpaRepository<LoginUserInfo, String> {
-}
